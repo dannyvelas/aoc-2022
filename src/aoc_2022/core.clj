99ddc5523065328d6481855885c75b2aa8b2df1b
Daniel Velasquez <danivelas4@gmail.com>
2022-12-03 01:01:31 -0500

    initial commit with lein scaffolding and day1 part1

---
+++ src/aoc_2022/core.clj
@@ -0,0 +1,19 @@
+(ns aoc-2022.core
+  (:gen-class)
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]))
+
+(defn add-up-vec [v]
+  (reduce + v))
+
+(defn int-vec [v]
+  (map #(Integer/parseInt %) v))
+
+(defn -main [& _]
+  (let [file-content (slurp (io/resource "calories.txt"))
+        calorie-groups (str/split file-content #"\n\n")
+        calorie-matrix (map #(str/split % #"\n") calorie-groups)
+        calorie-matrix-num (map int-vec calorie-matrix)
+        added-up-calories (map add-up-vec calorie-matrix-num)
+        max-calories (apply max added-up-calories)]
+    max-calories))
