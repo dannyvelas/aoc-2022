a5782f18036d45ad85435af8bc0796c1c1d34e91
Daniel Velasquez <danivelas4@gmail.com>
2022-12-04 22:22:07 -0500

    src/day2/part1/core.clj: small changes

--- src/day2/part1/core.clj
+++ src/day2/part1/core.clj
@@ -3,7 +3,7 @@
   (:require [clojure.java.io :as io]
             [clojure.string :as str]))
 
-(defn str-to-kw [c]
+(defn str-to-play [c]
   (case c
     ("A" "X") :rock
     ("B" "Y") :paper
@@ -28,8 +28,8 @@
   (let [file-content (slurp (io/resource "rock-paper-scissors.txt"))
         char-rounds (str/split file-content #"\n")
         char-rounds-matrix (mapv #(str/split % #" ") char-rounds)
-        kw-rounds-matrix (mapv #(mapv str-to-kw %) char-rounds-matrix)
-        play-&-res-vec (mapv (fn [round] [(second round) (apply get-res round)]) kw-rounds-matrix)
+        play-rounds-matrix (mapv #(mapv str-to-play %) char-rounds-matrix)
+        play-&-res-vec (mapv (fn [round] [(second round) (apply get-res round)]) play-rounds-matrix)
         pts-per-round (mapv (fn [[play res]] (+ (play-to-pts play) (res-to-pts res))) play-&-res-vec)
         net-points (reduce + pts-per-round)]
     net-points))
