b86f2d065b6fb0e9886aeb69deaab4ddca2addb3
Daniel Velasquez <danivelas4@gmail.com>
2022-12-08 18:15:21 -0500

    src/day4/part1/core.clj: completed

---
+++ src/day4/part1/core.clj
@@ -0,0 +1,22 @@
+(ns day4.part1.core
+  (:gen-class)
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]))
+
+(defn str-range-to-map [s]
+  (let [[start-str end-str] (str/split s #"-")]
+    {:start (Integer/parseInt start-str)
+     :end (Integer/parseInt end-str)}))
+
+(defn range-fully-contains? [r1 r2]
+  (<= (r1 :start) (r2 :start) (r2 :end) (r1 :end)))
+
+(defn -main [& _]
+  (let [file-content (slurp (io/resource "04-sections.txt"))
+        pairs-str (str/split file-content #"\n")
+        pairs-vec (mapv #(str/split % #",") pairs-str)
+        pairs-map (mapv #(mapv str-range-to-map %) pairs-vec)
+        pairs-full-overlap (filter #(or (range-fully-contains? (% 0) (% 1))
+                                        (range-fully-contains? (% 1) (% 0))) pairs-map)
+        amt-full-overlaps (count pairs-full-overlap)]
+    amt-full-overlaps))
