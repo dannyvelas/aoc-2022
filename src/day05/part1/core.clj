6a3cb79c65a55e20320b097c4aa181cf40e8d310
Daniel Velasquez <danivelas4@gmail.com>
2022-12-13 08:39:21 -0500

    src/day05/part1/core.clj: finished pushing part

--- src/day05/part1/core.clj
+++ src/day05/part1/core.clj
@@ -1,6 +1,10 @@
 (ns day05.part1.core
   (:gen-class)
-  (:require [clojure.set :as set]))
+  (:require [clojure.java.io :as io]
+            [clojure.set :as set]
+            [clojure.string :as str]))
+
+(def crate-str-len 4)
 
 (defn map-vals [f m]
   (into {} (for [[k v] m] [k (f v)])))
@@ -11,14 +15,12 @@
         remove-is (map-vals #(mapv (fn [m] (m :e)) %) groups-of-n)]
     remove-is))
 
-(defn push-crate-layer [stack-map layer]
-  (reduce-kv
-   (fn [agg i curr-stack]
-     (let [stack-to-add (if-let [new-crate (get layer i)]
-                          (conj curr-stack new-crate)
-                          curr-stack)]
-       (assoc agg i stack-to-add)))
-   {} stack-map))
+(defn push-one-layer [stack-map layer]
+  (into {} (for [[i stack] stack-map]
+             (let [stack-to-add (if-let [new-crate (get layer i)]
+                                  (conj stack new-crate)
+                                  stack)]
+               [i stack-to-add]))))
 
 (defn vec-diff [v1 v2]
   (let [v1-set (set v1)
@@ -27,19 +29,33 @@
 
 (defn line-to-indexed-crate-map [line]
   (let [as-char-vec (vec (char-array line))
-        separated-stacks (vec-to-groups as-char-vec 4)
+        separated-stacks (vec-to-groups as-char-vec crate-str-len)
         alpha-chars-only (map-vals #(vec-diff % [\[ \space \]]) separated-stacks)
         nonempty-vecs-only (into {} (filter #(seq (second %)) alpha-chars-only))
         vals-as-chars (map-vals first nonempty-vecs-only)]
     vals-as-chars))
 
-(defn init-stack-map [size]
-  (into {} (for [i (range size)] [i '()])))
+(defn stacks-section-to-vec-of-maps [stacks-section]
+  (let  [split-by-nl (str/split stacks-section #"\n")
+         no-col-names (subvec split-by-nl 0 (dec (count split-by-nl)))
+         vec-of-maps (mapv line-to-indexed-crate-map no-col-names)]
+    vec-of-maps))
+
+(defn calc-amt-stacks [stacks-section]
+  (let [single-line (get (str/split stacks-section #"\n") 0)]
+    (int (Math/ceil (/ (count single-line) crate-str-len)))))
+
+(defn push-all-layers [empty-stack-map layers]
+  (loop [i 0 stack-map empty-stack-map]
+    (cond (< i (count layers)) (let [new-stack-map (push-one-layer stack-map (get layers i))]
+                                 (recur (inc i) new-stack-map))
+          :else stack-map)))
 
 (defn -main [& _]
-  (let [line "            [J]             [B] [W]"
-        layer (line-to-indexed-crate-map line)
-        amt-stacks (int (Math/ceil (/ (count line) 4)))
-        stack-map (init-stack-map amt-stacks)
-        new-stack-map (push-crate-layer stack-map layer)]
-    new-stack-map))
+  (let [file-content (slurp (io/resource "05-crates.txt"))
+        [stacks-section instructions-section] (str/split file-content #"\n\n")
+        amt-stacks (calc-amt-stacks stacks-section)
+        empty-stack-map (into {} (for [i (range amt-stacks)] [i '()]))
+        layers (stacks-section-to-vec-of-maps stacks-section)
+        filled-stack-map (push-all-layers empty-stack-map layers)]
+    filled-stack-map))
